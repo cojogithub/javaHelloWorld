0d771106ff3ef5c0c5a8fac6f527da87df5aaa7f
cojogithub <christopherojo@aol.com>
2017-04-11 11:42:54 -0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        # this next line of codes are wrong!
+        int= wrong 
+            str=1
+            
     }
 
 }
